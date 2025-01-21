7f6552e7a38db41b9d021a1479746b83d30f923c
Nikson <nikolaking88@gmail.com>
2025-01-21 21:06:34 +0100

    #5 Created data source xml plugin and briefly tested it

--- data_source_plugin_xml/sok/graph/explorer/data_source_plugin_xml.py
+++
@@ -1,8 +0,0 @@
-from sok.graph.explorer.api.model.graph import Graph
-
-class DataSourcePluginXml(DataLoaderBase):
-    def identifier(self):
-        return "DataSourcePluginXml"
-
-    def name(self):
-        return "Data Source Plugin XML"
