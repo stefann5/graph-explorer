22ffb7883e1c1646ca38ba4b9299ae6fee1e7d49
Nikson <nikolaking88@gmail.com>
2025-01-23 00:13:18 +0100

    #16 Added selecting plugins without dinamic loading

--- platform/graph_explorer_platform/apps.py
+++ platform/graph_explorer_platform/apps.py
@@ -5,12 +5,47 @@ import pkg_resources
 class GraphExplorerPlatformConfig(AppConfig):
     name = 'graph_explorer_platform'
     data_source_plugins = []
+    selected_plugin = None
+    data_source = None
     data_visalizer_plugins = []
+
     def ready(self):
         self.data_source_plugins = load_plugins("graph.data_source")
         self.data_visalizer_plugins = load_plugins("graph.data_visualizer")
+        
+        if len(self.data_source_plugins) > 0:
+            self.selected_plugin = self.data_source_plugins[0]
+            if 'json' in self.data_source_plugins[0].__class__.__name__.lower():
+                self.data_source = 'test.json'
+            if 'xml' in self.data_source_plugins[0].__class__.__name__.lower():
+                self.data_source = 'test.xml'
+
+    def set_data_source_plugin(self, plugin_type):
+        """
+        Set the active data source plugin based on type string (xml/json)
+        Returns True if successful, False if plugin type not found
+        """
+        plugin_type = plugin_type.lower()
+        for plugin in self.data_source_plugins:
+            # Check class name for matching type
+            class_name = plugin.__class__.__name__.lower()
+            if ('json' in class_name and plugin_type == 'json') or \
+               ('xml' in class_name and plugin_type == 'xml'):
+                self.selected_plugin = plugin
+                if(plugin_type == 'json'):
+                    self.data_source = 'test.json'
+                if(plugin_type == 'xml'):
+                    self.data_source = 'test.xml'
+                return True
+        return False
+        
+    def get_current_plugin(self):
+            """Return currently selected plugin"""
+            return self.selected_plugin
+
     graph=None
 
+        
 def load_plugins(label):
     plugins = []
     for ep in entry_points(group=label):
--- platform/graph_explorer_platform/urls.py
+++ platform/graph_explorer_platform/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
+    path('select-plugin/', views.select_plugin, name="select_plugin"),
     path('search/', views.search, name="search_graph"),
     path('filter/', views.filter, name="filter_graph"),
 ]
